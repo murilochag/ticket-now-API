390f9c1a27018fb5da00aedf1e345ea9cb42f23c
murilochag <murilochagas10@outlook.com>
2024-02-07 11:25:57 -0300

    starting the project

---
+++ src/main/java/com/murilochag/ticketnowAPI/TicketNowApiApplication.java
@@ -0,0 +1,13 @@
+package com.murilochag.ticketnowAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketNowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilochag/ticketnowAPI/TicketNowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilochag.ticketnowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketNowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
